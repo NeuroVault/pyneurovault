6621b80c1875ade3ee4fa9345451b45277885298
vsoch <vsochat@stanford.edu>
2015-02-27 15:45:29 -0800

    modified:   examples/summarize_example.py

--- examples/summarize_example.py
+++ examples/summarize_example.py
@@ -2,7 +2,6 @@ from pyneurovault import api
 
 # Will extract all collections and images in one query to work from
 nv = api.NeuroVault()
-data = nv.get_images_with_collections_df()
 
 # Get unique cognitive atlas contrasts and counts
 nv.get_contrast_counts()
@@ -11,10 +10,11 @@ nv.get_modality_counts()
 nv.get_map_type_counts()
 nv.get_collection_counts()
 
-# Find images of a particular map type, Z images (also can specify modality)
-image_ids = nv.get_image_ids(map_type="Z",modality=None)
+# Find images of a particular map type, Z images (also can specify modality=)
+image_ids = nv.get_image_ids(map_type="Z")
 
 # Download images (also save output file of meta data)
-outfolder = "/scratch/users/russpold/data/NEUROVAULT/dump"
+outfolder = "/home/vanessa/Desktop/Z"
 standard = "/usr/share/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz"
-nv.download_and_resample(outfolder,standard,image_ids=image_ids)
+download_data = nv.download_and_resample(outfolder,standard,image_ids=image_ids)
+download_data.to_pickle("/home/vanessa/Desktop/Z/image_data.pkl")
