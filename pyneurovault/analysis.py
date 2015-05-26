ad866db7ccda434ab24c6464e78d6964e8dd5698
Yannick Schwartz <yannick.schwartz@cea.fr>
2015-05-26 15:21:51 +0200

    mainly pep8 fixes

--- pyneurovault/analysis.py
+++ pyneurovault/analysis.py
@@ -9,53 +9,49 @@ pyneurovault: a python wrapped for the neurovault api
 
 """
 
-__author__ = ["Poldracklab","Chris Filo Gorgolewski","Gael Varoquaux","Vanessa Sochat"]
+__author__ = ["Poldracklab", "Chris Filo Gorgolewski",
+              "Gael Varoquaux", "Vanessa Sochat"]
 __version__ = "$Revision: 1.0 $"
 __date__ = "$Date: 2015/01/16 $"
 __license__ = "BSD"
 
-import json
-import pandas as pd
+import os
+
 import numpy as np
-import pylab as plt
 import nibabel as nb
-import urllib, os
-from pyneurovault.utils import mkdir_p, get_url, get_standard_brain
-from nilearn.image import resample_img
-from pandas.io.json import json_normalize
-from urllib2 import Request, urlopen, HTTPError
+
+from pyneurovault.utils import get_standard_brain
 from nipype.utils.filemanip import split_filename
-from nilearn.plotting.img_plotting import plot_anat
-from nilearn.masking import compute_background_mask, _extrapolate_out_mask
 
 
 def get_frequency_map(images_df, dest_dir, target):
-  mask_img = get_standard_brain()
-  mask = nb.load(mask_img).get_data().astype(np.bool)
+    mask_img = get_standard_brain()
+    mask = nb.load(mask_img).get_data().astype(np.bool)
 
-  target_nii = nb.load(target)
-  resampled_path = os.path.join(dest_dir, "resampled")
-  freq_map_data = np.zeros(target_nii.shape)
+    target_nii = nb.load(target)
+    resampled_path = os.path.join(dest_dir, "resampled")
+    freq_map_data = np.zeros(target_nii.shape)
 
-  n_images = 0
-  for row in combined_df.iterrows():
-    _, _, ext = split_filename(row[1]['file'])
-    orig_file = os.path.join(resampled_path,"%06d%s" % (row[1]['image_id'], ext))
-    resampled_nii = nb.load(orig_file)
-    data = resampled_nii.get_data().squeeze()
-    data[np.isnan(data)] = 0
-    data[np.logical_not(mask)] = 0
-    data = np.abs(data)
+    n_images = 0
+    for row in images_df.iterrows():
+        _, _, ext = split_filename(row[1]['file'])
+        orig_file = os.path.join(
+            resampled_path, "%06d%s" % (row[1]['image_id'], ext))
+        resampled_nii = nb.load(orig_file)
+        data = resampled_nii.get_data().squeeze()
+        data[np.isnan(data)] = 0
+        data[np.logical_not(mask)] = 0
+        data = np.abs(data)
 
-    # Keep only things that are very significant
-    data = data > 3
-    if len(data.shape) == 4:
-      for d in np.rollaxis(data, -1):
-        freq_map_data += (d != 0)
-        n_images +=1
-    else:
-        freq_map_data += data
-        n_images += 1
+        # Keep only things that are very significant
+        data = data > 3
+        if len(data.shape) == 4:
+            for d in np.rollaxis(data, -1):
+                freq_map_data += (d != 0)
+                n_images += 1
+        else:
+            freq_map_data += data
+            n_images += 1
 
-  freq_map_data *= 100. / n_images
-  return nb.Nifti1Image(freq_map_data, target_nii.get_affine())
+    freq_map_data *= 100. / n_images
+    return nb.Nifti1Image(freq_map_data, target_nii.get_affine())
--- pyneurovault/utils.py
+++ pyneurovault/utils.py
@@ -11,86 +11,92 @@ pyneurovault: a python wrapped for the neurovault api
 import os
 import json
 import errno
-import urllib2
 import pandas
-import numpy as np
-from pandas.io.json import read_json
 from urllib2 import Request, urlopen, HTTPError
 
-__author__ = ["Poldracklab","Chris Filo Gorgolewski","Gael Varoquaux","Vanessa Sochat"]
+__author__ = ["Poldracklab", "Chris Filo Gorgolewski",
+              "Gael Varoquaux", "Vanessa Sochat"]
 __version__ = "$Revision: 1.0 $"
 __date__ = "$Date: 2015/01/16 $"
 __license__ = "BSD"
 
+CWD = os.path.abspath(os.path.split(__file__)[0])
+
 
 # Get standard brains
-
 def get_standard_brain():
-  cwd = os.path.abspath(os.path.split(__file__)[0])
-  return "%s/MNI152_T1_2mm_brain.nii.gz" %(cwd)
+    return os.path.join(CWD, 'data', 'MNI152_T1_2mm_brain.nii.gz')
+
 
 def get_standard_mask():
-  cwd = os.path.abspath(os.path.split(__file__)[0])
-  return "%s/MNI152_T1_2mm_brain_mask.nii.gz" %(cwd)
+    return os.path.join(CWD, 'data', 'MNI152_T1_2mm_brain_mask.nii.gz')
 
 
-# File operations 
-
+# File operations
 def mkdir_p(path):
-  try:
-      os.makedirs(path)
-  except OSError as exc: # Python >2.5
-    if exc.errno == errno.EEXIST and os.path.isdir(path):
-      pass
-    else: raise
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 
 def get_url(url):
-  request = Request(url)
-  response = urlopen(request)
-  return response.read()
+    request = Request(url)
+    response = urlopen(request)
+    return response.read()
 
-'''Return general json'''
+
 def get_json(url):
+    '''Return general json'''
     json_single = get_url(url)
     json_single = json.loads(json_single.decode("utf-8"))
     if json_single["count"] == 1:
         return json_single["results"]
     else:
-        print "Found %s results." %(json_single["count"])    
+        print "Found %s results." % json_single["count"]
         json_all = json_single["results"]
         while json_single["next"] is not None:
-             print "Retrieving %s" %(json_single["next"])
-             try:
-                 json_single = get_url(json_single["next"])
-                 json_single = json.loads(json_single.decode("utf-8"))
-                 json_all = json_all + json_single['results']
-             except HTTPError:
-                 print "Cannot get, retrying"
-        return json_all 
+            print "Retrieving %s" % json_single["next"]
+            try:
+                json_single = get_url(json_single["next"])
+                json_single = json.loads(json_single.decode("utf-8"))
+                json_all = json_all + json_single['results']
+            except HTTPError:
+                print "Cannot get, retrying"
+        return json_all
 
-'''Return paginated json data frame, for images and collections'''
-def get_json_df(data_type,pks=None,limit=1000):
+
+def get_json_df(data_type, pks=None, limit=1000):
+    '''Return paginated json data frame, for images and collections'''
     json_all = list()
-    if pks==None:  # May not be feasible call if database is too big
-         url = "http://neurovault.org/api/%s/?limit=%s&format=json" %(data_type,limit)
-         json_all = pandas.DataFrame(get_json(url))
+    if pks is None:  # May not be feasible call if database is too big
+        url = "http://neurovault.org/api/%s/?limit=%s&format=json" % (
+            data_type, limit)
+        json_all = pandas.DataFrame(get_json(url))
 
     else:
-        if isinstance(pks,str): pks = [pks]
-        if isinstance(pks,int): pks = [pks]
+        if isinstance(pks, str):
+            pks = [pks]
+        elif isinstance(pks, int):
+            pks = [pks]
         json_all = "["
-        for p in range(0,len(pks)):
+        for p in range(0, len(pks)):
             pk = pks[p]
-            print "Retrieving %s %s..." %(data_type[0:-1],pk)
+            print "Retrieving %s %s..." % (data_type[0:-1], pk)
             try:
-                tmp = get_url("http://neurovault.org/api/images/%s/?format=json" %(pk))
-                if p!=0:
-                    json_all = "%s,%s" %(json_all,tmp)
+                tmp = get_url("http://neurovault.org/api"
+                              "/images/%s/?format=json" % pk)
+                if p != 0:
+                    json_all = "%s,%s" % (json_all, tmp)
                 else:
-                    json_all = "%s%s" %(json_all,tmp)
+                    json_all = "%s%s" % (json_all, tmp)
             except:
-                print "Cannot retrieve %s %s, skipping." %(data_type[0:-1],pk)
-            
-        json_all = "%s]" %(json_all)
+                print "Cannot retrieve %s %s, skipping." % (
+                    data_type[0:-1], pk)
+
+        json_all = "%s]" % json_all
         json_all = pandas.DataFrame(json.loads(json_all.decode("utf-8")))
     return json_all
