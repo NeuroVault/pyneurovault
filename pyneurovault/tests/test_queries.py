441e717884fde7e0257c366650d911b3575a52b6
vsoch <vsochat@stanford.edu>
2017-02-09 23:26:41 -0500

    modified:   pyneurovault/tests/test_queries.py

--- pyneurovault/tests/test_queries.py
+++ pyneurovault/tests/test_queries.py
@@ -16,13 +16,22 @@ from nose.tools import (
 )
 
 from pyneurovault.api import collections_from_dois
+import unittest
 
-'''Test REST API Queries'''
-def test_queries():
-    doi = "10.1016/j.neurobiolaging.2012.11.002"
+class TestQueries(unittest.TestCase):
 
-    # A DOI is associated with a collection. Download it.
-    collection = collections_from_dois(doi)
-    assert_equal(collection.shape[0],1)
-    collections = collections_from_dois([doi,doi])
-    assert_equal(collections.shape[0],2)
+    def setUp(self):
+        print("\n---START----------------------------------------")
+        
+    def tearDown(self):
+        print("\n---END------------------------------------------")
+
+    def test_queries(self):
+        '''Test REST API Queries'''
+        doi = "10.1016/j.neurobiolaging.2012.11.002"
+
+        # A DOI is associated with a collection. Download it.
+        collection = collections_from_dois(doi)
+        assert_equal(collection.shape[0],1)
+        collections = collections_from_dois([doi,doi])
+        assert_equal(collections.shape[0],2)
